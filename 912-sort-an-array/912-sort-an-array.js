1e14fce06cf29e5102064d1aef7a6750216d03c9
Andrey Kolesnik <andrii_kolesnyk@epam.com>
2022-08-02 22:05:12 +0200

    Time: 8177 ms (6.69%), Space: 51.4 MB (98.01%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.js
+++ 912-sort-an-array/912-sort-an-array.js
@@ -5,24 +5,16 @@
 var sortArray = function(nums) {
     let n = nums.length;
         
-    for(let i = 0; i < n; i++) {
-        // Finding the smallest number in the subarray
-        let min = i;
-        for(let j = i+1; j < n; j++){
-            if(nums[j] < nums[min]) {
-                min=j; 
+    for(let i = 0; i < n; i++) {        
+        for(let j = i+1; j < n; j++)
+            if (nums[i] > nums[j]) {
+                 const tmp = nums[i]; 
+                 nums[i] = nums[j];
+                 nums[j] = tmp;      
             }
-         }
-         if (min != i) {
-             // Swapping the elements
-             let tmp = nums[i]; 
-             nums[i] = nums[min];
-             nums[min] = tmp;      
-        }
+        
     }
-   
     
     return nums;
-    
     
 };
