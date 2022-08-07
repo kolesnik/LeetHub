909d6b8b111d8087380b8b5b8b4d97a6fa59be3e
Andrey Kolesnik <andrii_kolesnyk@epam.com>
2022-08-07 16:12:54 +0200

    Time: 67 ms (86.96%), Space: 42.1 MB (59.43%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let start = 0
+    let end = nums.length - 1;
+    
+    while(start < end) {
+        const mid = Math.floor((start + end)/2);
+        if(nums[mid] === target)
+            return mid;
+        
+        target > nums[mid] ? start = mid + 1: end = mid;   
+    }
+    
+    if(start === end)
+        return nums[start] >= target? start: start + 1
+};
